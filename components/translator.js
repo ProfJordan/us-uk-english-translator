6f0b201279e7ef87e0f2c2e00b0b8ef5048cbe5d
ProfJordan <Jordan.Halliday@slcc.edu>
2024-01-17 14:38:22 -0700

    updated functions and logic

--- components/translator.js
+++ components/translator.js
@@ -4,7 +4,101 @@ const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
 class Translator {
+    toBritishEnglish(text) {
+        const dict = { ...americanOnly, ...americanToBritishSpelling };
+        const titles = americanToBritishTitles;
+        const timeRegex = /([1-9]|1[012]):[0-5][0-9]/g;
+        const translated = this.translate(
+            text,
+            dict,
+            titles,
+            timeRegex,
+            "toBritish"
+        );
+        if (!translated) {
+            return text;
+        }
 
+        return translated;
+    }
+
+    toAmericanEnglish(text) {
+        const dict = { ...britishOnly, ...reverseDict(americanToBritishSpelling) };
+        const titles = reverseDict(americanToBritishTitles);
+        const timeRegex = /([0-9]|1[012]).[0-5][0-9]/g;
+        const translated = this.translate(
+            text,
+            dict,
+            titles,
+            timeRegex,
+            "toAmerican"
+        );
+        if (!translated) {
+            return text;
+        }
+    }
+    translate(text, dict, titles, timeRegex, locale) {
+        const lowerText = text.toLowerCase();
+        const matchesMap = {};
+
+        // Search for titles/honorific & add to matchesMap
+        Object.entries(titles).map(([k, v]) => {
+            if (lowerText.includes(k)) {
+                matchesMap[k] = v.charAt(0).toUpperCase() + v.slice(1);
+            }
+        });
+// filter words with spaces from current dictionary
+const wordsWithSpace = Object.fromEntries(
+    Object.entries(dict).filter(([k, v]) => k.includes(" "))
+);
+
+// search for spaced word matches & add to matchesMap
+Object.entries(wordsWithSpace).map(([k, v]) => {
+    if (lowerText.inludes(k)) (matchesMap[k] = v)
+}); 
+// search for individual word matches & add to matchesMap
+    lowerText.match(/(\w+([-'])(\w+)?['-]?(\w+))|\w+/g).forEach((word) => {
+if (dict[word]) matchesMap[word] = dict[word];        
+    });
+    // search for time matches & add to matchesMap
+    const matchedTimes = lowerText.match(timeRegex);
+
+    if (matchedTimes) {
+        matchedTimes.map((e) => {
+        if (locale === "toBritish") {
+            return (matchesMap[e]) = e.replace(":", ".");
+        }
+        return (matchesMap[e] = e.replace(".", ":"));
+        });
+    }
+
+    //no matches
+    if (Object.keys(matchesMap).length === 0) return null;
+     // Return logic
+     console.log("matchesMap :>> ", matchesMap);
+     const translation = this.replaceAll(text, matchesMap);
+     const translationWithHighlight = this.replaceAllWithHighlight(
+text,
+matchesMap
+     );
+
+     return [translation, translationWithHighlight];
+    }
+
+    replaceAll(text, matchesMap) {
+        // matchesMap :>> { favorite: 'favourite' }
+        // text :>> Mangoes are my favorite fruit.
+        const re = new RegExp(Object.keys(matchesMap).join("|"), "gi");
+        return text.replace(re, (matched) => matchesMap[matched.toLowerCase()]);
+    }
+    replaceAllWithHighlight(text, matchesMap) {
+        const re = new RegExp(Object.keys(matchesMap).join("|"), "gi");
+        return text.replace(re, (matched) => {
+            return `<span class="highlight">${matchesMap[matched,toLowerCase()]
+            }</span>`;
+        });
+    }
 }
+
 
 module.exports = Translator;
