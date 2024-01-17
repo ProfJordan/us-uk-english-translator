0001304678d4d95e7e2664f6c6bed671fe202150
ProfJordan <Jordan.Halliday@slcc.edu>
2024-01-17 14:43:18 -0700

    added reverseDict function for the dictionary files

--- components/translator.js
+++ components/translator.js
@@ -3,6 +3,12 @@ const americanToBritishSpelling = require('./american-to-british-spelling.js');
 const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
+const reverseDict = (obj) => {
+    return Object.assign(
+        {},
+        ...Object.entries(obj).map(([k, v]) => ({ [v]: k }))
+    );
+};
 class Translator {
     toBritishEnglish(text) {
         const dict = { ...americanOnly, ...americanToBritishSpelling };
